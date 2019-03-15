c93b3ea59b412744c8c8e45456f8a73233bcb838
ranchu09 <imonocd@icloud.com>
2019-03-15 09:26:02 +0800

    delete unused api

--- src/api/newApi/sv_supervision.js
+++ src/api/newApi/sv_supervision.js
@@ -1,36 +1,5 @@
 import request from "../../utils/request";
 
-export function supervisions(pageNum, pageSize,currentBankArea, currentCity, bankKind, bankType,
-                            businessCategory, accountType, orgaCode, bankEntryUserCode,
-                            bankReviewUserCode, renEntryUserCode, renRecheckUserCode,
-                            transactionNum, approvalCode, identifier, startTime, endTime) {
-    const params = {
-        pageSize:pageSize,
-        pageNum:pageNum,
-        currentBankArea:currentBankArea,
-        currentCity:currentCity,
-        bankKind:bankKind,
-        bankType:bankType,
-        businessCategory:businessCategory,
-        accountType:accountType,
-        orgaCode:orgaCode,
-        bankEntryUserCode:bankEntryUserCode,
-        bankReviewUserCode:bankReviewUserCode,
-        renEntryUserCode:renEntryUserCode,
-        renRecheckUserCode:renRecheckUserCode,
-        transactionNum:transactionNum,
-        approvalCode:approvalCode,
-        identifier:identifier,
-        startTime:startTime,
-        endTime:endTime
-    };
-    return request({
-        url: '/supervision/supervision',
-        method: 'get',
-        params:params
-    });
-}
-
 export function supervisionsWithPage(data) {
     return request({
         url: '/supervision/supervisions',
